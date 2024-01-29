4e64524cafcb543498b0bc7a5b1cd94a23dc3043
iPokrZ <i.pokrzywniak1@universityofgalway.ie>
2024-01-29 23:47:57 +0000

    Update header.js

--- forum-app/src/stores/header.js
+++ forum-app/src/stores/header.js
@@ -1,14 +1,9 @@
 import { defineStore } from 'pinia'
 
 export const useHeaderStore = defineStore('header', {
-  state: () => ({ count: 0, name: 'Home' }),
-  getters: {
-    doubleCount: (state) => state.count * 2,
-  },
+  state: () => ({ name: 'Home' }),
+  getters: {},
   actions: {
-    increment() {
-      this.count++
-    },
     updateHeaderName(newName){
       console.log('Updating header name:', newName);
       this.name = newName;
