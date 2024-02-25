720eedd68fd720b6a3c9a1d0db417c6777b27ade
iPokrZ <i.pokrzywniak1@universityofgalway.ie>
2024-02-25 20:31:47 +0000

    Adding popup forms

--- forum-app/src/components/AppHeader.vue
+++ forum-app/src/components/AppHeader.vue
@@ -9,25 +9,85 @@
       <h1 style="color: white;">{{ message }}</h1>
     </div>
 
+<<<<<<< Updated upstream
     <!-- Navigation Menu -->
     <div class="menu-section">
       <NavigationMenu />
+=======
+    <div class="container">
+
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+
+        <div class="logo-section">
+          <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+            <img src="@/assets/AlumnPSD-LogoOnly.png" alt="Site Logo" class="logo" href="/"/>
+          </a>
+
+        </div>
+
+        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+          <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+        </a>
+
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center align-items-center mb-md-0 mx-auto text-center fs-4">
+          <li :class="{ 'active': $route.path === '/' }">
+            <router-link to="/" class="nav-link px-2 text-white ">Home</router-link>
+           </li>
+
+          <li  :class="{ 'active': $route.path === '/about' }">
+            <router-link to="/about" class="nav-link px-2 text-white">About</router-link>
+          </li>
+          <li :class="{ 'active': $route.path === '/community-finder' }">
+            <router-link to="/community-finder" class="nav-link px-2 text-white">Communities</router-link>
+          </li>
+          <li :class="{ 'active': $route.path === '/topics' }">
+            <router-link to="/topics" class="nav-link px-2 text-white">Topics</router-link>
+          </li>
+          <li :class="{ 'active': $route.path === '/chatrooms' }">
+            <router-link to="/chatrooms" class="nav-link px-2 text-white">Chatrooms</router-link>
+          </li>
+          <li :class="{ 'active': $route.path === '/quiz' }">
+            <router-link to="/quiz" class="nav-link px-2 text-white">Quiz</router-link>
+          </li>
+        </ul>
+        
+        <LoginForm/>
+        <SignUpForm/>
+
+            <!-- Navigation Menu -->
+          <div class="menu-section">
+            <NavigationMenu/>
+
+          </div>
+      </div>
+>>>>>>> Stashed changes
     </div>
   </header>
 </template>
 
 <script>
 import NavigationMenu from './NavigationMenu.vue';
+<<<<<<< Updated upstream
+=======
+import SignUpForm from './SignUpForm.vue';
+import LoginForm from './LoginForm.vue';
+>>>>>>> Stashed changes
 
 export default {
-  components: {
-    NavigationMenu
-  },
-  props: {
-    message: String,
-  },
   
-}
+  props: ['TogglePopup'],
+
+  components: {
+    NavigationMenu,
+    SignUpForm,
+    LoginForm
+  }
+  //Props are no longer needed since we pass nothing into the header
+  //props: {
+   // message: String,
+ // },
+  
+};
 </script>
 
 
---
+++ forum-app/src/components/SignUpForm.vue
@@ -0,0 +1,103 @@
+<template>
+
+<modal class="SignUp">
+  <div class="container">
+  <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#signupform">
+    Sign-Up
+  </button>  
+</div>
+
+<div class="modal fade" id="signupform" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header border-bottom-0">
+        <h5 class="modal-title" id="exampleModalLabel"> Create Account </h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <form>
+        <div class="modal-body">
+          <div class="form-group">
+            <label for="email1">Email address</label>
+            <input type="email" class="form-control" id="email1" aria-describedby="emailHelp" placeholder="Enter email">
+            <small id="emailHelp" class="form-text text-muted">Your information is safe with us.</small>
+          </div>
+          <div class="form-group">
+            <label for="password1">Password</label>
+            <input type="password" class="form-control" id="password1" placeholder="Password">
+          </div>
+          <div class="form-group">
+            <label for="password1">Confirm Password</label>
+            <input type="password" class="form-control" id="password2" placeholder="Confirm Password">
+          </div>
+        </div>
+        <div class="modal-footer border-top-0 d-flex justify-content-center">
+          <button type="submit" class="btn btn-success">Submit</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+</modal>
+</template>
+
+
+
+<style scoped>
+/* Style for the navigation bar {name: 'about', params: {message: 'Hi there' }} */
+.navbar {
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  padding: 0.5rem;
+  box-sizing: border-box;
+}
+
+/* Style for the dropdown menu */
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+/* Style for the dropdown button */
+.dropbtn {
+  background-color: #4CAF50;
+  color: white;
+  padding: 10px;
+  border: none;
+  cursor: pointer;
+}
+
+.modal-title{
+  color: black;
+}
+/* Style for the dropdown content */
+.dropdown-content {
+  display: none;
+  position: absolute;
+  right: 0;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+/* Style for links within the dropdown, stacked vertically */
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+/* Style for hover effect on links */
+.dropdown-content a:hover {
+  background-color: #f1f1f1;
+}
+
+/* Display the dropdown content when hovering over the dropdown button */
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+</style>
