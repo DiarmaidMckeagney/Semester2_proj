4804238a33b0c75a1dff40724894ac7b6419f29e
DiarmaidMckeagney <117989065+DiarmaidMckeagney@users.noreply.github.com>
2024-02-28 16:13:01 +0000

    adding getiing chatrooms names

--- forum-app/src/components/ChatroomsPage.vue
+++ forum-app/src/components/ChatroomsPage.vue
@@ -6,21 +6,15 @@
       <!-- Chatroom List Section -->
       <section style="width: 66%; margin-right: 4%;">
         <h2>What's New in Chatrooms</h2>
-        <ul>
+        <ul v-for="n in chatrooms.length">
           <!-- List of Chatrooms -->
           <li style="border: 1px solid #ccc; margin-bottom: 10px; padding: 10px; background-color: #f0f0f0;">
-            <span>Chatroom 1</span>
-            <button class="join-button" style="background-color: #333; color: white;">Join</button>
-          </li>
-          <li style="border: 1px solid #ccc; margin-bottom: 10px; padding: 10px; background-color: #f0f0f0;">
-            <span>Chatroom 2</span>
-            <button class="join-button" style="background-color: #333; color: white;">Join</button>
-          </li>
-          <li style="border: 1px solid #ccc; margin-bottom: 10px; padding: 10px; background-color: #f0f0f0;">
-            <span>Chatroom 3</span>
+            <span>{{ chatrooms[n-1] }}</span>
             <button class="join-button" style="background-color: #333; color: white;">Join</button>
           </li>
           <!-- Create New Chatroom -->
+        </ul>
+        <ul>
           <li style="border: 1px solid #ccc; margin-bottom: 10px; padding: 10px; background-color: #f0f0f0;">
             <span>Create New Chat</span>
             <button class="join-button" style="background-color: #333; color: white;">Create</button>
@@ -36,6 +30,34 @@
     </main>
   </div>
 </template>
+
+<script>
+import app from '../api/firebase';
+import {getFunctions, httpsCallable} from "firebase/functions";
+
+
+export default {
+  data() {
+    return {
+      chatrooms:[]
+    }
+  },
+  created() {
+    this.chatroomNames();
+  },
+  methods: {
+    chatroomNames() {
+      const functions = getFunctions(app);
+      const chatroomNames = httpsCallable(functions, 'chatroomNames');
+      chatroomNames().then((result) => {
+        console.log(result);
+        this.chatrooms = result.data;
+      })
+    },
+  }
+}
+
+</script>
 
 <style scoped>
 /* Add your CSS here */
